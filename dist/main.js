6a29328acb325dbd2111897760b0f393367f78e5
MSA <crystalbluetech@gmail.com>
2023-05-17 01:06:30 +0100

    Add Javascript-menu control

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,28 @@
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.desktop-menu');
+// Dom  items
+const header = document.querySelector('#header');
+const burger = document.querySelector('#burger');
 
-function toogleMenu(){
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('active');
+let showMenu = false;
+
+function toggleMenu() {
+  if (!showMenu) {
+    header.classList.remove('header');
+    header.classList.add('header-active');
+    burger.classList.remove('burger');
+    burger.classList.add('burger-active');
+    document.body.style.overflowY = 'hidden';
+
+    // set  menu state
+    showMenu = true;
+  } else {
+    header.classList.remove('header-active');
+    header.classList.add('header');
+    burger.classList.remove('burger-active');
+    burger.classList.add('burger');
+    document.body.style.overflowY = 'auto';
+
+    // Set menu state
+    showMenu = false;
+  }
 }
-
-menuBtn.addEventListener('click', toogleMenu);
-menuBtn.addEventListener('touchstart', toogleMenu);
+burger.addEventListener('click', toggleMenu);
