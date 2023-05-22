445abc5122ba5f4a595a38736bbbab17d77a45fd
MSA <crystalbluetech@gmail.com>
2023-05-22 21:18:13 +0100

    Add Project deetails in array

---
+++ dist/detailed-project.js
@@ -0,0 +1,63 @@
+// Define the project data in an array
+const projects = [
+    {
+        name: 'Tonic',
+        description:
+            'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+        image: './img/projects/Snapshoot_Portfolio_1.png',
+        technologies: ['HTML', 'CSS', 'JavaScript',],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/mohashyne/MSalyhu_portfolio.git',
+        projectCover: './img/projects/Snapshoot_Portfolio_1.png',
+        fullDescription: `
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum tempore aliquam similique aperiam officia soluta dolorum neque velit aspernatur odit libero animi quod deserunt praesentium accusantium possimus ad iste sed ratione, aut vel repellendus obcaecati quia.
+          `,
+        canopy: ['Backend DEV', 2023],
+        client: 'CANOPY',
+    },
+    {
+        name: 'Multi-Post <br> Stories',
+        description:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+        image: './img/projects/Snapshoot_Portfolio_2.png',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/mohashyne/MSalyhu_portfolio.git',
+        projectCover: './img/projects/Snapshoot_Portfolio_2.png',
+        fullDescription: `
+              Multi-Post A daily selection of privately personalized reads; no accounts or sign-ups required, no accounts or sign-ups.
+          `,
+        canopy: ['Backend DEV', 2022],
+        client: 'FACEBOOK',
+    },
+    {
+        name: 'Tonic',
+        description:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+        image: './img/projects/Snapshoot_Portfolio_3.png',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/mohashyne/MSalyhu_portfolio.git',
+        projectCover: './img/projects/Snapshoot_Portfolio_3.png',
+        fullDescription: `
+                Multi-Post A daily selection of privately personalized reads; no accounts or sign-ups required, no accounts or sign-ups.
+            `,
+        canopy: ['Backend DEV', 2022],
+        client: 'FACEBOOK',
+    },
+    {
+        name: 'Multi-Post <br> Stories',
+        description:
+            "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+        image: './img/projects/Snapshoot_Portfolio_4.png',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        liveLink: 'https://example.com',
+        sourceLink: 'https://github.com/mohashyne/MSalyhu_portfolio.git',
+        projectCover: './img/projects/Snapshoot_Portfolio_4.png',
+        fullDescription: `
+                Multi-Post A daily selection of privately personalized reads; no accounts or sign-ups required, no accounts or sign-ups.
+            `,
+        canopy: ['Backend DEV', 2022],
+        client: 'UBER',
+    },
+];
