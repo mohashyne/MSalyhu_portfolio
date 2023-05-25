9dc7cc0049b1cb73f7996a7821202fdb1ff4b1c9
MSA <crystalbluetech@gmail.com>
2023-05-25 06:36:06 +0100

    Update styling

--- dist/detailed-project.js
+++ dist/detailed-project.js
@@ -35,11 +35,11 @@ const projects = [
     client: 'CANOPY',
     description:
             'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: './img/projects/Snapshoot_Portfolio_3.svg',
+    image: './img/projects/Snapshoot_Portfolio_3.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     liveLink: 'https://example.com',
     sourceLink: 'https://github.com/mohashyne/MSalyhu_portfolio.git',
-    projectCover: './img/projects/Snapshoot_Portfolio_3.svg',
+    projectCover: './img/projects/Snapshoot_Portfolio_3.png',
     fullDescription: `
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum tempore aliquam similique aperiam officia soluta dolorum neque velit aspernatur odit libero animi quod deserunt praesentium accusantium possimus ad iste sed ratione, aut vel repellendus obcaecati quia.
           `,
