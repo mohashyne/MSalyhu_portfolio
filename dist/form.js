83a7c36a2568deff2e830db93f7ddef11b8b2a6d
MSA <crystalbluetech@gmail.com>
2023-05-26 07:11:27 +0100

    fix linter errors

--- dist/form.js
+++ dist/form.js
@@ -1,21 +1,18 @@
 const form = document.querySelector('#form');
 const feedBack = document.querySelector('.feedback');
 
-
-
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // validation
   const fullName = form.fullname.value;
   const fullNamePattern = /^[A-Za-z]{6,20}$/;
-  if(fullNamePattern.test(fullName)){
+  if (fullNamePattern.test(fullName)) {
     feedBack.textContent = `${fullName} is  valid`;
     feedBack.style.color = '#fff';
   } else {
-    feedBack.textContent = `${fullName} is  inValid`
+    feedBack.textContent = `${fullName} is  inValid`;
     form.fullname.style.border = '1px solid red';
     feedBack.style.color = 'red';
   }
-
-})
+});
